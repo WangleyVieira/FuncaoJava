ce7502dc40c5fb019dfc6c5edfc0435cb93addda
Wangley Vieira <wangleymsv10@hotmail.com>
2020-04-02 20:17:58 -0300

    Adicionando Função Calculadora

---
+++ src/funcaocalculadoraprincipal/Funcao.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package funcaocalculadoraprincipal;
+
+/**
+ *
+ * @author Msv Wangley
+ */
+public class Funcao {
+    public static double soma(double n1, double n2){
+        double resultSoma = n1 + n2;
+        return resultSoma;
+    }
+    
+    public static double subtracao(double n1, double n2){
+        double resultSubtracao = n1 - n2;
+        return resultSubtracao;
+    }
+    
+    public static double divisao(double n1, double n2){
+        double resultDivisao = n1 / n2;
+        return resultDivisao;
+    }
+    
+    public static double multiplicacao(double n1, double n2){
+        double resultMultiplicacao = n1 * n2;
+        return resultMultiplicacao;
+    }
+    
+}
